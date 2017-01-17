213518707db7499aad52fcf661a0d1b79bd1790b
Ronando Long <ronando.long@gmail.com>
2017-01-17 15:40:47 -0800

    add page for user input and will execute getters and setters for rectangle object

--- app/app.php
+++ app/app.php
@@ -1,32 +1,37 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/rectangle.php";
 
     $app = new Silex\Application();
 
-    $app->get("/", function() {
-        return
-        "<!DOCTYPE html>
-        <html>
-        <head>
-            <title>Hello Friend!</title>
-            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
-        </head>
-        <body>
-            <div class='container'>
-                <h1>Hello From Afar</h1>
-                <p>Dear Friend,</p>
-                <p>How are you? I hope that you are having a nice weekend. I'm vacationing in the mountains of Tibet while I learn programming! </p>
-                <p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>
-                <p>But I like programming a lot, so I've got that going for me. </p>
-                <p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>
-                <p>Cheers,</p>
-                <p>Travel Enthusiast Jane</p>
-                <br>
-                <a href='/goodbye'>Goodbye friend</a>
-            </div>
-        </body>
-        </html>";
+    $app->get("/new_rectangle", function() {
+      return "
+      <!DOCTYPE html>
+      <html>
+      <head>
+          <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+          <title>Make a rectangle!</title>
+      </head>
+      <body>
+          <div class='container'>
+              <h1>Geometry Checker</h1>
+              <p>Enter the dimensions of your rectangle to see if it's a square.</p>
+              <form action='/view_rectangle'>
+                  <div class='form-group'>
+                    <label for='length'>Enter the length:</label>
+                    <input id='length' name='length' type='number'>
+                  </div>
+                  <div class='form-group'>
+                    <label for='width'>Enter the width:</label>
+                    <input id='width' name='width' type='number'>
+                  </div>
+                  <button type='submit' class='btn-success'>Create</button>
+              </form>
+          </div>
+      </body>
+      </html>
+      ";
     });
 
     $app->get("/goodbye", function() {
---
+++ src/rectangle.php
@@ -0,0 +1,51 @@
+<?php
+  class Rectangle  // Class Declaration
+  {
+    private $length; // made these private
+    private $width;
+
+    function __construct($side_length, $side_width)
+    {
+        $this->length = $side_length;
+        $this->width = $side_width;
+    }
+
+    function isSquare()  // method to calculate if square boolean
+    {
+        if ($this->length == $this->width) {
+          return true;
+        } else {
+          return false;
+        }
+    }
+
+    function area()  // method to calculate area
+    {
+        return $this->length * $this->width;
+    }
+
+// SET LENGTH
+    function setLength($new_length)  // Sets the value of the object's property length
+    {
+        $this->lenth = (float) $new_length;
+    }
+
+// GET LENGTH
+    function getLength($new_length)
+    {
+        return $this->length;
+    }
+// SET WIDTH
+    function setWidth($new_width)
+    {
+        $this->width = (float) $new_width;
+    }
+// GET WIDTH
+    function getWidth()
+    {
+        return $this->width;
+    }
+
+  }
+
+?>
